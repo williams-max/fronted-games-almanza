7b42bb5cc8abefb49d0a7255835975242bb5ed38
Israel <almanzaisrael75@gmail.com>
2024-03-02 20:18:03 -0400

    guadardado de nodes en base de datos

--- src/app/(routes)/components/customNode.tsx
+++ src/app/(routes)/components/customNode.tsx
@@ -1,8 +1,8 @@
 import { useInputNodeStore } from "@/store/nodes-store";
 import { ChangeEvent, useCallback, useState, useRef, useEffect } from "react";
 import { Handle, Position } from "reactflow";
-// import TextareaAutosize from 'react-textarea-autosize';
-import TextareaAutosize from 'react-autosize-textarea';
+import TextareaAutosize from 'react-textarea-autosize';
+// import TextareaAutosize from 'react-autosize-textarea';
 import { ChromePicker } from 'react-color'; // Importa ChromePicker de react-color
 // import PaletteIcon from '@mui/icons-material/Palette'; // Importa el icono de paleta de colores de Material Icons
 import ColorLensIcon from '@mui/icons-material/ColorLens';
@@ -36,21 +36,15 @@ export function TextUpdaterNode() {
   );
 }
 
-export function TextDemoNode(props: any) {
+export function NodeCentral(props: any) {
 
   const [isResizing, setIsResizing] = useState(false);
-  const [backgroundColor, setBackgroundColor] = useState('#0064A5');
-  const [textColor, setTextColor] = useState('#ffffff');
+  const [backgroundColor, setBackgroundColor] = useState<any>('#0064A5');
+  const [textColor, setTextColor] = useState<any>('#ffffff');
   const [showColorPicker, setShowColorPicker] = useState(false); // Estado para mostrar/ocultar la paleta de colores
   const [showTextColorPicker, setShowTextColorPicker] = useState(false); // Estado para mostrar/ocultar la paleta de colores para el texto
   /// const textareaRef = useRef(null); 
-  
-  const handleResize = useCallback((e) => {
-
-  console.log('with', e.target.clientWidth)
-   // e.target.clientHeight
- 
-  }, []);
+  const textAreaRef = useRef(null);
 
   const handleMouseEnter = useCallback(() => {
     setIsResizing(true);
@@ -62,23 +56,33 @@ export function TextDemoNode(props: any) {
 
   const handleBackgroundColorChange = (color: any) => {
     setBackgroundColor(color.hex);
-    setValue('backgroundColor',color.hex)
+    // setValue('backgroundColor',color.hex)
   };
 
   const handleTextColorChange = (color: any) => {
     setTextColor(color.hex);
-    setValue('textColor',color.hex)
+    //setValue('textColor',color.hex)
   };
 
-  const { control, nameRegister, register, getValues , setValue } = props.data.methods
-  const { id } = props
-  //console.log('propiedades',props)
-
-  const valores = () => {
-
-  }
+  const { control, getValues , setValue } = props.data.methods
+  const { width , height , fondoColor , textoColor} = props.data
+  const [vertical ,setVertical] = useState('400px');
+  const [horizontal ,setHorizantal] = useState();
+  const { id} = props  
   useEffect ( () => {
     setValue(id,props.data.content)
+    if(height) {
+      setVertical(height)
+    }
+    if(width){
+      setHorizantal(width)
+    }
+    if(fondoColor){
+      setBackgroundColor(fondoColor);
+    }
+    if(textoColor){
+      setTextColor(textoColor)
+    }    
   },[])
 
   return (
@@ -92,6 +96,7 @@ export function TextDemoNode(props: any) {
             <TextareaAutosize
               placeholder="Text..."
               {...field}
+              ref= {textAreaRef}
               onResize={e =>  console.log(e)}
               onChange={(e) => {
                 field.onChange((e.target.value))
@@ -104,10 +109,13 @@ export function TextDemoNode(props: any) {
                     // Si coincide, actualiza la data del objeto
                     return {
                       ...objeto, // Mantén todas las propiedades originales
-                      width: props?.width, 
                       data: { // Actualiza la propiedad 'data' como desees
                         ...objeto.data,
-                        content: e.target.value
+                        content: e.target.value,
+                        width: `${textAreaRef.current.clientWidth}px`, // Accessing width
+                        height: `${textAreaRef.current.clientHeight}px`, // Accessing height
+                        fondoColor : backgroundColor , 
+                        textoColor : textColor
                       }
                     };
                   } else {
@@ -121,8 +129,8 @@ export function TextDemoNode(props: any) {
               }}
               className={isResizing ? "nodrag" : ""}
               style={{
-                width: '100%',
-                padding: '8px',
+                width: horizontal ? `${horizontal}` :'100%',
+                height: vertical ? vertical : '200px',
                 borderRadius: '5px',
                 backgroundColor: backgroundColor,
                 color: textColor, // Aplica el color de texto seleccionado
@@ -184,7 +192,329 @@ export function TextDemoNode(props: any) {
           <ChromePicker color={textColor} onChange={handleTextColorChange} />
         </div>
       )}
-      <button onClick={() => valores()}> print valores</button>
+      {/*<button onClick={() => valores()}> print valores</button> */}
+    </>
+  );
+}
+
+export function NodeSuperior(props: any) {
+
+  const [isResizing, setIsResizing] = useState(false);
+  const [backgroundColor, setBackgroundColor] = useState<any>('#C00101');
+  const [textColor, setTextColor] = useState<any>('#ffffff');
+  const [showColorPicker, setShowColorPicker] = useState(false); // Estado para mostrar/ocultar la paleta de colores
+  const [showTextColorPicker, setShowTextColorPicker] = useState(false); // Estado para mostrar/ocultar la paleta de colores para el texto
+  /// const textareaRef = useRef(null); 
+  const textAreaRef = useRef(null);
+
+  const handleMouseEnter = useCallback(() => {
+    setIsResizing(true);
+  }, []);
+
+  const handleMouseLeave = useCallback(() => {
+    setIsResizing(false);
+  }, []);
+
+  const handleBackgroundColorChange = (color: any) => {
+    setBackgroundColor(color.hex);
+    // setValue('backgroundColor',color.hex)
+  };
+
+  const handleTextColorChange = (color: any) => {
+    setTextColor(color.hex);
+    //setValue('textColor',color.hex)
+  };
+
+  const { control, getValues , setValue } = props.data.methods
+  const { width , height , fondoColor , textoColor} = props.data
+  const [vertical ,setVertical] = useState('200px');
+  const [horizontal ,setHorizantal] = useState();
+  const { id} = props  
+  useEffect ( () => {
+    setValue(id,props.data.content)
+    if(height) {
+      setVertical(height)
+    }
+    if(width){
+      setHorizantal(width)
+    }
+    if(fondoColor){
+      setBackgroundColor(fondoColor);
+    }
+    if(textoColor){
+      setTextColor(textoColor)
+    }    
+  },[])
+
+  return (
+    <>
+      <div style={{ border: '2px solid #0064A5', borderRadius: '8px', padding: '0px', margin: '0px', width: '100%', height: '100%' }}>
+        <Controller
+          name={id}
+          render={({ field }: any) => (
+            // Cambiamos el input por un textarea
+
+            <TextareaAutosize
+              placeholder="Text..."
+              {...field}
+              ref= {textAreaRef}
+              onResize={e =>  console.log(e)}
+              onChange={(e) => {
+                field.onChange((e.target.value))
+                console.log('print form nodes ',getValues('nodes'))
+                const nodesTemp = getValues('nodes')
+                // Suponiendo que tienes tu array llamado `miArray` y el id que estás buscando es "textDemo_node_0"
+                // Utiliza map para recorrer el array y actualizar el objeto si el id coincide
+                const nuevoArray = nodesTemp.map((objeto: any) => {
+                  if (objeto.id === id) {
+                    // Si coincide, actualiza la data del objeto
+                    return {
+                      ...objeto, // Mantén todas las propiedades originales
+                      data: { // Actualiza la propiedad 'data' como desees
+                        ...objeto.data,
+                        content: e.target.value,
+                        width: `${textAreaRef.current.clientWidth}px`, // Accessing width
+                        height: `${textAreaRef.current.clientHeight}px`, // Accessing height
+                        fondoColor : backgroundColor , 
+                        textoColor : textColor
+                      }
+                    };
+                  } else {
+                    return objeto;
+                  }
+                });
+                setValue('nodes',nuevoArray)
+                console.log('valor de array ', nuevoArray)
+                console.log(getValues('nodes'))
+                console.log('----------------------------')
+              }}
+              className={isResizing ? "nodrag" : ""}
+              style={{
+                width: horizontal ? `${horizontal}` :'100%',
+                height: vertical ? vertical : '200px',
+                borderRadius: '5px',
+                backgroundColor: backgroundColor,
+                color: textColor, // Aplica el color de texto seleccionado
+                resize: 'both'
+              }}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            />
+          )}
+          control={control}
+        />
+
+        <Handle type="target" position={Position.Bottom} id="input_target" />
+        <Handle type="source" position={Position.Top} id="input_source" />
+      </div>
+      <button
+        style={{
+          fontSize: '20px',
+          margin: '0px',
+          padding: '0px',
+          border: 'none',
+          lineHeight: '1',
+          cursor: 'pointer',
+          backgroundColor: 'transparent',
+          color: '#0064A5',
+          position: 'absolute',
+          top: '-23px',
+          left: '10px' // Ajuste de posición del botón de la paleta de colores
+        }}
+        onClick={() => setShowColorPicker(!showColorPicker)}
+      >
+        <ColorLensIcon sx={{ color: 'white' }} />
+      </button>
+      <button
+        style={{
+          fontSize: '20px',
+          margin: '0px',
+          padding: '0px',
+          border: 'none',
+          lineHeight: '1',
+          cursor: 'pointer',
+          backgroundColor: 'transparent',
+          color: '#0064A5',
+          position: 'absolute',
+          top: '-23px',
+          left: '40px' // Ajuste de posición del botón de cambio de color de texto PL
+        }}
+        onClick={() => setShowTextColorPicker(!showTextColorPicker)} // Mostrar la paleta de colores para el texto
+      >
+        <ColorizeIcon sx={{ color: 'white' }} />
+      </button>
+      {showColorPicker && (
+        <div style={{ position: 'absolute', zIndex: 2, resize: 'block' }} className={"nodrag"}>
+          <ChromePicker color={backgroundColor} onChange={handleBackgroundColorChange} />
+        </div>
+      )}
+      {showTextColorPicker && (
+        <div style={{ position: 'absolute', zIndex: 2, resize: 'block', left: '40px' }} className={"nodrag"}>
+          <ChromePicker color={textColor} onChange={handleTextColorChange} />
+        </div>
+      )}
+      {/*<button onClick={() => valores()}> print valores</button> */}
+    </>
+  );
+}
+
+export function NodeInferior(props: any) {
+
+  const [isResizing, setIsResizing] = useState(false);
+  const [backgroundColor, setBackgroundColor] = useState<any>('#00AE9E');
+  const [textColor, setTextColor] = useState<any>('#ffffff');
+  const [showColorPicker, setShowColorPicker] = useState(false); // Estado para mostrar/ocultar la paleta de colores
+  const [showTextColorPicker, setShowTextColorPicker] = useState(false); // Estado para mostrar/ocultar la paleta de colores para el texto
+  /// const textareaRef = useRef(null); 
+  const textAreaRef = useRef(null);
+
+  const handleMouseEnter = useCallback(() => {
+    setIsResizing(true);
+  }, []);
+
+  const handleMouseLeave = useCallback(() => {
+    setIsResizing(false);
+  }, []);
+
+  const handleBackgroundColorChange = (color: any) => {
+    setBackgroundColor(color.hex);
+    // setValue('backgroundColor',color.hex)
+  };
+
+  const handleTextColorChange = (color: any) => {
+    setTextColor(color.hex);
+    //setValue('textColor',color.hex)
+  };
+
+  const { control, getValues , setValue } = props.data.methods
+  const { width , height , fondoColor , textoColor} = props.data
+  const [vertical ,setVertical] = useState('400px');
+  const [horizontal ,setHorizantal] = useState();
+  const { id} = props  
+  useEffect ( () => {
+    setValue(id,props.data.content)
+    if(height) {
+      setVertical(height)
+    }
+    if(width){
+      setHorizantal(width)
+    }
+    if(fondoColor){
+      setBackgroundColor(fondoColor);
+    }
+    if(textoColor){
+      setTextColor(textoColor)
+    }    
+  },[])
+
+  return (
+    <>
+      <div style={{ border: '2px solid #0064A5', borderRadius: '8px', padding: '0px', margin: '0px', width: '100%', height: '100%' }}>
+        <Controller
+          name={id}
+          render={({ field }: any) => (
+            // Cambiamos el input por un textarea
+
+            <TextareaAutosize
+              placeholder="Text..."
+              {...field}
+              ref= {textAreaRef}
+              onResize={e =>  console.log(e)}
+              onChange={(e) => {
+                field.onChange((e.target.value))
+                console.log('print form nodes ',getValues('nodes'))
+                const nodesTemp = getValues('nodes')
+                // Suponiendo que tienes tu array llamado `miArray` y el id que estás buscando es "textDemo_node_0"
+                // Utiliza map para recorrer el array y actualizar el objeto si el id coincide
+                const nuevoArray = nodesTemp.map((objeto: any) => {
+                  if (objeto.id === id) {
+                    // Si coincide, actualiza la data del objeto
+                    return {
+                      ...objeto, // Mantén todas las propiedades originales
+                      data: { // Actualiza la propiedad 'data' como desees
+                        ...objeto.data,
+                        content: e.target.value,
+                        width: `${textAreaRef.current.clientWidth}px`, // Accessing width
+                        height: `${textAreaRef.current.clientHeight}px`, // Accessing height
+                        fondoColor : backgroundColor , 
+                        textoColor : textColor
+                      }
+                    };
+                  } else {
+                    return objeto;
+                  }
+                });
+                setValue('nodes',nuevoArray)
+                console.log('valor de array ', nuevoArray)
+                console.log(getValues('nodes'))
+                console.log('----------------------------')
+              }}
+              className={isResizing ? "nodrag" : ""}
+              style={{
+                width: horizontal ? `${horizontal}` :'100%',
+                height: vertical ? vertical : '200px',
+                borderRadius: '5px',
+                backgroundColor: backgroundColor,
+                color: textColor, // Aplica el color de texto seleccionado
+                resize: 'both'
+              }}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            />
+          )}
+          control={control}
+        />
+
+        <Handle type="target" position={Position.Bottom} id="input_target" />
+        <Handle type="source" position={Position.Top} id="input_source" />
+      </div>
+      <button
+        style={{
+          fontSize: '20px',
+          margin: '0px',
+          padding: '0px',
+          border: 'none',
+          lineHeight: '1',
+          cursor: 'pointer',
+          backgroundColor: 'transparent',
+          color: '#0064A5',
+          position: 'absolute',
+          top: '-23px',
+          left: '10px' // Ajuste de posición del botón de la paleta de colores
+        }}
+        onClick={() => setShowColorPicker(!showColorPicker)}
+      >
+        <ColorLensIcon sx={{ color: 'white' }} />
+      </button>
+      <button
+        style={{
+          fontSize: '20px',
+          margin: '0px',
+          padding: '0px',
+          border: 'none',
+          lineHeight: '1',
+          cursor: 'pointer',
+          backgroundColor: 'transparent',
+          color: '#0064A5',
+          position: 'absolute',
+          top: '-23px',
+          left: '40px' // Ajuste de posición del botón de cambio de color de texto PL
+        }}
+        onClick={() => setShowTextColorPicker(!showTextColorPicker)} // Mostrar la paleta de colores para el texto
+      >
+        <ColorizeIcon sx={{ color: 'white' }} />
+      </button>
+      {showColorPicker && (
+        <div style={{ position: 'absolute', zIndex: 2, resize: 'block' }} className={"nodrag"}>
+          <ChromePicker color={backgroundColor} onChange={handleBackgroundColorChange} />
+        </div>
+      )}
+      {showTextColorPicker && (
+        <div style={{ position: 'absolute', zIndex: 2, resize: 'block', left: '40px' }} className={"nodrag"}>
+          <ChromePicker color={textColor} onChange={handleTextColorChange} />
+        </div>
+      )}
+      {/*<button onClick={() => valores()}> print valores</button> */}
     </>
   );
 }
--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -18,23 +18,29 @@ import ReactFlow, {
 
 import "reactflow/dist/style.css";
 
-import { TextUpdaterNode, TextDemoNode } from "./components/customNode";
+import { TextUpdaterNode, NodeCentral , NodeSuperior , NodeInferior } from "./components/customNode";
 import { ContextMenu } from "./components/contextMenu";
+import { v4 as uuidv4 } from 'uuid';
 
 import { useInputNodeStore } from "@/store/nodes-store";
-import DataNodes from './DataNodes.json'
-import DataEdges from './DataEdges.json'
+import axios from 'axios'
 
 let id = 0;
-const getId = (type: string) => `${type}_node_${id++}`;
+//const getId = (type: string) => `${type}_node_${id++}`;
 
-export default function Home() {
- 
+const getId = () => {
+  let getIdx = uuidv4();
+  console.log('get id', getIdx)
+  return getIdx;
+}
+
+export default function DiagramaRender() {
+
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
   const { formState, handleSubmit, control, register, getValues, setValue, unregister, reset } = useForm();
-   
 
+  const urlBase = 'http://localhost:3003'
   // store handling for input nodes
   const { text } = useInputNodeStore();
 
@@ -48,10 +54,10 @@ export default function Home() {
       setNodes((nds) => [
         ...nds,
         {
-          id: getId(type),
+          id: getId(), // getId(type),
           backgroundColor: '#0064A5',
           textColor: '#ffffff',
-          data: { content: '' , methods : {register, control , getValues , setValue : setValue}},
+          data: { content: '', methods: { register, control, getValues, setValue: setValue } ,height:'200px' },
           position: { x: 0, y: 0 },
           type: type,
         },
@@ -60,36 +66,84 @@ export default function Home() {
     [setNodes]
   );
 
- useEffect ( () => {
-    
-    console.log('data nodes', DataNodes)
-    console.log('data edges', DataEdges)
-    if(DataNodes && DataEdges){
-     const tempNodes = DataNodes.map((objeto: any) => {
-      if (objeto.hasOwnProperty('data')) {
-        return {
-          ...objeto,
-          data: {
-            ...objeto.data,
-            methods: {
-              register,
-              control,
-              getValues,
-              setValue
-            }
-          }
-        };
-      }
-      return objeto;
-    });
-      console.log('temp ', tempNodes)
-      setNodes(tempNodes)
-      setEdges(DataEdges)
-    setValue('nodes',DataNodes)
-    setValue('edges',DataEdges)
-    }
-  },[]) 
+  useEffect(() => {
+    loadData()
+  }, [])
 
+  const loadData = async () => {
+    try {
+      const response = await axios.get(`${urlBase}/componente/7171272e-b31b-4c34-9220-9f535c958c5c`)
+      if (response.data.datos.nodes) {
+        const tempNodes = response.data.datos.nodes.map((objeto: any) => {
+          if (objeto.hasOwnProperty('data')) {
+            return {
+              ...objeto,
+              data: {
+                ...objeto.data,
+                methods: {
+                  register,
+                  control,
+                  getValues,
+                  setValue
+                }
+              }
+            };
+          }
+          return objeto;
+        });
+        setNodes(tempNodes)
+        setEdges(response.data.datos.edges)
+        setValue('nodes', tempNodes)
+        setValue('edges', response.data.datos.edges)
+        // console.log('nodes-->' , getValues())
+      }
+    } catch (error) {
+
+    }
+  }
+
+  const updateComonente = async () => {
+    try {
+      console.log(getValues())
+      // crear un un nodes actulazaldo este decir la posicon del node
+      // 1 iterear array nodes (que contine la posicion actulal de node)
+      // 1 iterear array nodes 
+      // tomar en encuenta en en nodesReferenicia tiene la mayor informacion por cada item en data')
+      // seria actulizar array nodes con algunos atributos de arrayReferencia ,(item) como data que el mas importante y este exite 
+      // en arrayReferencia , para este caso ambos array tiene el mismo id por item , pero dar prioridad 
+      /// a array nodes
+
+      // Supongamos que tienes los arrays nodes y nodesReferencia
+      const nodesReferencia = getValues('nodes')
+      // Utiliza map() para crear un nuevo array con los nodos actualizados
+      const nodesActualizados = nodes.map(node => {
+        // Busca el elemento correspondiente en nodesReferencia usando el mismo ID
+        const nodeReferencia = nodesReferencia.find((item: any) => item.id === node.id);
+
+        // Si se encontró un elemento correspondiente en nodesReferencia
+        // y tiene información adicional, actualiza el nodo de nodes
+        if (nodeReferencia && nodeReferencia.data) {
+          // Retorna un nuevo objeto con los atributos actualizados
+          return {
+            ...node, // Mantiene los atributos originales de node
+            data: nodeReferencia.data // Actualiza el atributo 'data' con la información de nodeReferencia
+            // Puedes añadir más atributos aquí según tus necesidades
+          };
+        } else {
+          // Si no se encontró una referencia o no tiene información adicional, devuelve el nodo original
+          return node;
+        }
+      });
+
+      const response = await axios.put(`${urlBase}/componente/7171272e-b31b-4c34-9220-9f535c958c5c`, {
+        nodes: nodesActualizados, // getValues('nodes'),
+        edges: edges
+      })
+      console.log('repuesta update ', response)
+    } catch (error) {
+      console.log('imprimiendo error ', error)
+    }
+  }
 
   // function that's called each time when you make connections
   const onConnect = useCallback(
@@ -144,7 +198,7 @@ export default function Home() {
   };
 
   // in order to add new nodes, we need to add them to the nodeTypes object
-  const nodeTypes = useMemo(() => ({ textUpdater: TextUpdaterNode, textDemo: TextDemoNode }), []);
+  const nodeTypes = useMemo(() => ({ textUpdater: TextUpdaterNode, nodeCentral: NodeCentral , nodeSuperior: NodeSuperior , nodeInferior:NodeInferior }), []);
 
   // open node context menu
   const onNodeContextMenu = useCallback(
@@ -231,22 +285,31 @@ export default function Home() {
           <Button
             onClick={() =>
               addNewNode({
-                type: "textDemo"
+                type: "nodeSuperior"
               })
             }
           >
-            Add new node
+            Add node(Sección superior)
           </Button>
-          <Button onClick={() => {
-            console.log('diagra actual  ', nodes)
-          }}>
-            Guardar 
+          <Button
+            onClick={() =>
+              addNewNode({
+                type: "nodeCentral"
+              })
+            }
+          >
+            Add node(Sección central)
           </Button>
-          <Button onClick={() => { 
-           //  setValue('nodes',nodes)
-           //  setValue('edges',edges)
-            //descargarJSON(getValues('nodes'))
-            console.log(getValues())}}>Guardar informacion</Button>
+          <Button
+            onClick={() =>
+              addNewNode({
+                type: "nodeInferior"
+              })
+            }
+          >
+            Add node(Sección inferior)
+          </Button>
+          <Button onClick={async () => updateComonente()}>Save</Button>
         </div>
         <ReactFlow
           ref={ref}
